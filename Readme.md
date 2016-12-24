34c72f66eb18acd9915ca68d87bdb93c3e4792fa
Setu Shubham <setushubham@yahoo.com>
2016-12-24 18:19:58 +0530

    gasstationlocator

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Gas Station Locator - Udacity Neighborhood Map Project
+
+Gas station Locator enables you to find Gas stations present in your city. It has been made using Google map API, Foursquare API and knockout js.
+
+## To run project
+
+Click here to run : http://setu08.github.io/gas-station-locator/
+
+## Installation
+
+You can simply clone or download zip from above. Open index.html in your browser to see the project.
+
+## Requirement to run the project
+
+* Internet connection required to access google map API and Foursquare API.
+* Updated browser.
---
+++ js/myapp.js
@@ -0,0 +1,210 @@
+var initialization = {
+        location:
+				{
+            lat: 28.7041,
+            lng: 77.1048973
+        },
+        radius: 180,
+        query: 'Gas Station'
+    },
+    map,
+    infoWindow,
+    marker, markers = [],
+    venueClick = {},
+		foursquareuri = '',
+    display = '',
+    venueFilter = [],
+    vMarkerPos;
+
+var viewModel = function() {
+    //scope alias
+    var self = this;
+    var geocoder = new google.maps.Geocoder();
+		document.getElementById('search-me').addEventListener('click', function() {
+        self.geoAddr(geocoder, map);
+    });
+    self.geoAddr = function(geocoder, resultsMap) {
+            var address = document.getElementById('location').value;
+            geocoder.geocode({
+                'address': address
+            }, function(results, status) {
+                if (status === google.maps.GeocoderStatus.OK) {
+                    resultsMap.setCenter(results[0].geometry.location);
+
+                    initialization.location = {
+                        lat: results[0].geometry.location.lat(),
+                        lng: results[0].geometry.location.lng()
+                    };
+                    self.searchTxt('');
+                    self.placesArray([]);
+                    self.clearMarkers();
+                    markers = [];
+                    self.runQuery(map);
+                } else {
+                    alert('Geocode was not successful for the following reason: ' + status);
+                }
+            });
+        },
+        self.runQuery = function(map) {
+            //passing a callback method,to handle the results object and a google.maps.places.PlacesServiceStatus response.
+            var service = new google.maps.places.PlacesService(map);
+            service.textSearch(initialization, self.callback);
+        },
+        self.callback = function(result, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                for (i = 0, len = result.length; i < len; i++) {
+                    self.pushVenue(result[i]);
+                    self.createMarker(result[i]);
+                }
+            }
+        },
+        self.pushVenue = function(place) {
+            self.placesArray.push(place.name);
+        },
+        //creating PlacesServiceStatus.textSearch() method, mareker and infowindow
+        self.createMarker = function(place) {
+            loc = place.geometry.location;
+            marker = new google.maps.Marker({
+                position: loc,
+                animation: google.maps.Animation.DROP,
+                map: map,
+                icon: 'image/marker.png'
+            });
+
+            self.callAjax(place, marker);
+            markers.push(marker);
+        },
+
+        self.callAjax = function(place, marker) {
+            // Setting up foursquar IDs
+            var ClientID = 'YDFWPRMR2KZOS15HMNLNAFG0IBLPGE4JSTATT1KMKBD445DE',
+                ClientSecret = 'PFCVGZFD34KCK1SA4KYY4LDQFBTXO2KQ1OR0SG0GO21XFM20'
+
+            //FourSquare Search API
+            foursquareuri = 'https://api.foursquare.com/v2/venues/search?client_id=' + ClientID + '&client_secret=' + ClientSecret + '&v=20161219&ll=' + place.geometry.location.lat() +
+                ',' + place.geometry.location.lng() +
+                '&query=' + place.name + '&limit=1';
+
+            $.getJSON(foursquareuri, function(data) {
+                placeDetail = data.response.venues[0];
+
+                if (placeDetail) {
+                    self.placeAddress(placeDetail.location.hasOwnProperty('address') ? placeDetail.location.address : 'Address not found');
+
+                    self.placeLink(placeDetail.hasOwnProperty('url') ? placeDetail.url : 'No url Found');
+
+                    if (self.placeLink() === 'No url Found') {
+                        display = '<span class="gas-station">' + place.name + '</span>' + '<div class="gasdealer-detail">Gas Dealer Detail on Foursquare:<span class="foursquare-detail"><p class="address"> Address: ' + self.placeAddress() + '</p>' +
+                            '<span>URL: ' + self.placeLink() + '</span>' + '<p class="category"> Category: ' + placeDetail.categories[0].name + '</p></span></div>';
+                    } else {
+                        display = '<span class="gas-station">' + place.name + '</span>' + '<div class="gasdealer-detail">Gas Dealer Detail on Foursquare:<span class="foursquare-detail"><p class="address">Address: ' + self.placeAddress() + '</p>' +
+                            '<a href=' + self.placeLink() + '>' + self.placeLink() + '</a>' + '<p class="Category"> Category: ' + placeDetail.categories[0].name + '</p></span></div>';
+                    }
+                } else {
+                    console.log(place.name + ' not found');
+                    display = '<span class="gas-station">' + place.name + '</span>' + '<div class="gasdealer-detail">Place Not found in foursquare database</div>';
+                }
+                marker.displayProp = display;
+                google.maps.event.addListener(marker, 'click', function(marker, display) {
+                    return function() {
+                        //setting bounce animation
+                        marker.setAnimation(google.maps.Animation.BOUNCE);
+                        setTimeout(function() {
+                            marker.setAnimation(null);
+                        }, 1200);
+                        infoWindow.setContent(display);
+                        infoWindow.open(map, this);
+                    };
+                }(marker, display));
+
+            }).error(function(e) {
+                document.getElementsByClassName("noresponse").innerHTML = "<h2>Sorry, No Response from Foursquare.</h2>"
+            });
+        },
+
+        /* observable properties for data-binding, search function and event handlers*/
+        self.searchTxt = ko.observable(''),
+        self.placesArray = ko.observableArray([]),
+        self.markerArray = ko.observableArray([]),
+        self.placeAddress = ko.observable(''),
+        self.placeLink = ko.observable(''),
+        self.allMarkers = function(map) {
+            for (var i = 0; i < markers.length; i++) {
+                markers[i].setMap(map);
+            }
+        },
+        self.oneMarker = function(pos) {
+            markers[pos].setMap(map);
+            self.markerArray.push(markers[pos]);
+
+        },
+        self.clearMarkers = function() {
+            self.allMarkers(null);
+            self.markerArray([]);
+        },
+        self.filteredSearch = function() {
+            var filter = self.searchTxt().toLowerCase();
+            if (!filter) {
+                self.allMarkers(map);
+                self.markerArray(markers);
+                return self.placesArray();
+            } else {
+                self.clearMarkers();
+                venueFilter = [];
+                //knockout's utility fuction have been used here for live searching
+                return ko.utils.arrayFilter(this.placesArray(), function(venue) {
+                    if (venue.toLowerCase().indexOf(filter.toLowerCase()) >= 0) {
+                        //puts venues to other array
+                        venueFilter.push(venue);
+                        //will get venue positions
+                        vMarkerPos = self.placesArray().indexOf(venue);
+                        //new marker for all  filter venues
+                        self.oneMarker(vMarkerPos);
+                        return venueFilter;
+                    }
+                });
+            }
+        },
+        self.overVenueItem = function(index) {
+            venueClick = self.markerArray()[index];
+            if (venueClick.getAnimation() !== null) {
+                venueClick.setAnimation(null);
+            } else {
+                venueClick.setAnimation(google.maps.Animation.BOUNCE);
+            }
+        },
+        self.outVenueItem = function(index) {
+            venueClick.setAnimation(null);
+        },
+        self.clickVenueItem = function(index) {
+            venueClick = self.markerArray()[index];
+
+            infoWindow.setContent(venueClick.displayProp);
+            infoWindow.open(map, venueClick);
+
+
+            venueClick.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function() {
+                venueClick.setAnimation(null);
+            }, 750);
+        };
+
+    google.maps.event.addDomListener(window, 'load', self.runQuery(map));
+};
+
+function initMap() {
+    "use strict";
+    var mapOptions = {
+            center: initialization.location,
+            zoom: 12
+        },
+        elemMap = document.getElementById('my-map');
+    map = new google.maps.Map(elemMap, mapOptions);
+    infoWindow = new google.maps.InfoWindow();
+    google.maps.event.addDomListener(window, 'resize', function() {
+        var center = map.getCenter();
+        google.maps.event.trigger(map, 'resize');
+        map.setCenter(center);
+    });
+    ko.applyBindings(new viewModel());
+}
